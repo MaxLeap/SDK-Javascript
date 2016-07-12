b879f6bf9818d7c2918b4166b79e6a961e2d0aea
henry.bai <ybai@ilegendsoft.com>
2016-07-12 15:40:02 +0800

    add track origin event

--- test/analytics.js
+++ test/analytics.js
@@ -99,4 +99,41 @@ describe('Analytics', function () {
     })
   });
 
+  describe('#TrackOriginEvent', function(){
+    it('should track origin event', function(done){
+      var options = {
+        appId: appId,
+        userId: userId,
+        appVersion: '1.0'
+      };
+      var analytics = new ML.Analytics(options);
+      var data = [{
+        "distinct_id": "2b0a6f51a3cd6775",
+        "time": 1434556935000,
+        "type": "track",
+        "event": "ViewProduct",
+        "properties": {
+          "_manufacturer":"Apple",
+          "_model": "iPhone 5",
+          "_os":"iOS",
+          "_os_version":"7.0",
+          "_app_version":"1.3",
+          "_wifi":true,
+          "_ip":"180.79.35.65",
+          "_province":"湖南",
+          "_city":"长沙",
+          "_screen_width":320,
+          "_screen_height":640,
+          "product_id":123451231231212,
+          "product_name":"苹果",
+          "product_classify":"水果",
+          "product_price":14.0
+        }
+      }];
+      analytics.trackOriginEvent(data).then(function(res){
+        expect(res.errorCode).to.be(0);
+        done();
+      }).catch(done);
+    });
+  });
 });
